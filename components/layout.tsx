3c2ad254cf8b427f3ce6abef6fd145ab3da03339
Tibo <tbll75@gmail.com>
2022-12-12 09:48:58 +0800

    Add Google Tag Manager to the site
    This will allow us to track page views and other events.

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import { Footer } from "./footer";
 import * as Analytics from "utils/analytics";
+import Script from "next/script";
 
 interface Props {
   title?: string;
@@ -25,6 +26,18 @@ export function Layout({ children }: Props) {
         <link rel="apple-touch-icon" sizes="128x128" href="/logo/logo_128.png"></link>
         <link rel="icon" sizes="192x192" href="/logo/logo_192.png"></link>
         <link rel="canonical" href={url} />
+
+        {/* GOOGLE TAG MANAGER - TAPLIO */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NCK7Z8K');
+          `}
+        </Script>
+        
         <meta name="description" content={desc} />
         {/* Twitter */}
         <meta name="twitter:card" content="summary" key="twcard" />
@@ -57,6 +70,9 @@ export function Layout({ children }: Props) {
           }}
         />
       </Head>
+      {/* GOOGLE TAG MANAGER - TAPLIO */}
+      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCK7Z8K" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+      {/* <Header /> */}
       <Box>{children}</Box> 
       <Footer />
     </Box>
